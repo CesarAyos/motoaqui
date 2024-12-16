2089528b1e9367b0bad2184d2bb809906a55441e
CesarAyos <ayos007@gmail.com>
2024-12-16 00:32:10 -0400

    modify

--- src/routes/profileUser/+page.svelte
+++ src/routes/profileUser/+page.svelte
@@ -32,7 +32,36 @@
   });
 
   
+
+  
 </script>
+
+<div
+  class="alert alert-primary alert-dismissible fade show"
+  role="alert"
+>
+  <button
+    type="button"
+    class="btn-close"
+    data-bs-dismiss="alert"
+    aria-label="Close"
+  ></button>
+  <ol>
+   <li> <strong>Bienvenido</strong> Estos son los pasos para pedir un motoaqui</li>
+   <li> <strong>Selecciona manualmente</strong> en donde estas ubicado y tu destino</li>
+   <li> <strong>Elige la moneda</strong> en la que quieres cancelar</li>
+   <li> <strong>Indicanos si debemos llevar vueltos</strong> y dinos que billete tienes</li>
+   <li> <strong>Dinos en que tiempo quieres</strong> que te busquemos</li>
+   <li> <strong>Dale pedir carrera</strong> esto nos enviara un mensaje de whatsapp</li>
+   <li> <strong>Vuelve a la aplicacion</strong> </li>
+   <li> <strong>Dale click en </strong> Chequear carrera </li>
+   <li> <strong>Dale click en  </strong>  recargar </li>
+   <li> <strong>En unos instantes te aparecera </strong>Los datos necesario de tu conductor asignado </li>
+   <li> <strong>Ten un feliz viaje </strong> recomiendanos </li>
+  </ol>
+</div>
+
+
 
 
 <div>
