0fec117a94cfd7df58de7192e026cb572f7d1847
CesarAyos <ayos007@gmail.com>
2024-12-24 23:51:29 -0400

    modify

--- src/components/carrerasAsignadas.svelte
+++ src/components/carrerasAsignadas.svelte
@@ -1,9 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { supabase } from "../components/supabase.js";
-  import protegerRuta from "../components/protegerRuta.js";
-  
-  
+  import { protegerRuta } from "../components/protegerRuta.js";
 
   let carrerasAsignadas = [];
 
@@ -49,7 +47,6 @@
   onMount(async () => {
     await protegerRuta();
     await cargarCarrerasAsignadas();
-    
   });
 
   // Función para recargar las carreras
@@ -63,56 +60,57 @@
   };
 </script>
 
-<main >
-<div class="card" style="background: #1b1b1b;">
-  <div class="card-header d-flex justify-content-between text-white">
-    Carreras aceptadas
-    <button class="btn btn-outline-warning" on:click={recargarCarrerasAsignadas}
-      >Recargar</button
+<main>
+  <div class="card" style="background: #1b1b1b;">
+    <div class="card-header d-flex justify-content-between text-white">
+      Carreras aceptadas
+      <button
+        class="btn btn-outline-warning"
+        on:click={recargarCarrerasAsignadas}>Recargar</button
+      >
+    </div>
+    <button class="btn btn-danger m-2" type="button" on:click={handleLogout}
+      >Cerrar Sesión</button
     >
-  </div>
-  <button class="btn btn-danger m-2" type="button" on:click={handleLogout}
-    >Cerrar Sesión</button
-  >
-  <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-    <div class="col">
-      <div class=" card-body">
-        <blockquote class="blockquote mb-0">
-          {#if carrerasAsignadas.length > 0}
-            {#each carrerasAsignadas as carrera}
-              <div class="card mb-3">
-                <div class="card-body">
-                  <h5 class="card-title">Carrera ID: {carrera.id}</h5>
-                  <p class="card-text">Estado: {carrera.estado}</p>
-                  <p class="card-text">Usuario: {carrera.usuario_nombre}</p>
-                  {#if carrera.conductor}
-                    <p class="card-text">
-                      Conductor: {carrera.conductor.primernombre}
-                      {carrera.conductor.primerapellido}
-                    </p>
-                    <p class="card-text">
-                      Teléfono: {carrera.conductor.telefono}
-                    </p>
-                    <p class="card-text">
-                      Modelo de la moto: {carrera.conductor.modelo}
-                    </p>
-                    <p class="card-text">Placa: {carrera.conductor.placa}</p>
-                    <p class="card-text">Color: {carrera.conductor.color}</p>
-                    <p class="card-text">
-                      Control: {carrera.conductor.control}
-                    </p>
-                  {/if}
+    <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+      <div class="col">
+        <div class=" card-body">
+          <blockquote class="blockquote mb-0">
+            {#if carrerasAsignadas.length > 0}
+              {#each carrerasAsignadas as carrera}
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <h5 class="card-title">Carrera ID: {carrera.id}</h5>
+                    <p class="card-text">Estado: {carrera.estado}</p>
+                    <p class="card-text">Usuario: {carrera.usuario_nombre}</p>
+                    {#if carrera.conductor}
+                      <p class="card-text">
+                        Conductor: {carrera.conductor.primernombre}
+                        {carrera.conductor.primerapellido}
+                      </p>
+                      <p class="card-text">
+                        Teléfono: {carrera.conductor.telefono}
+                      </p>
+                      <p class="card-text">
+                        Modelo de la moto: {carrera.conductor.modelo}
+                      </p>
+                      <p class="card-text">Placa: {carrera.conductor.placa}</p>
+                      <p class="card-text">Color: {carrera.conductor.color}</p>
+                      <p class="card-text">
+                        Control: {carrera.conductor.control}
+                      </p>
+                    {/if}
+                  </div>
                 </div>
-              </div>
-            {/each}
-          {:else}
-            <p>No hay carreras asignadas.</p>
-          {/if}
-        </blockquote>
+              {/each}
+            {:else}
+              <p>No hay carreras asignadas.</p>
+            {/if}
+          </blockquote>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </main>
 
 <style>
@@ -138,7 +136,3 @@
     margin-left: auto;
   }
 </style>
-
-
-
-  
