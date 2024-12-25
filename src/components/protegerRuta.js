92a74eb451e54ef52261d68498e7ed333ff6d4ba
CesarAyos <ayos007@gmail.com>
2024-12-24 23:49:49 -0400

    modify

--- src/components/protegerRuta.js
+++ src/components/protegerRuta.js
@@ -1,14 +1,9 @@
+import { supabase } from "../components/supabase.js";
 
-  // protegerRuta.js
-  import { supabase } from "../components/supabase.js"; // Ajusta la ruta según sea necesario
-
-  export const protegerRuta = async () => {
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.getSession();
-    if (error || !session || !session.user) {
-      console.error("Error fetching session or user is not logged in:", error);
-      window.location.href = "/loginUser"; // Redirigir al login si no está autenticado
-    }
-  };
+export const protegerRuta = async () => {
+  const { data: { session }, error } = await supabase.auth.getSession();
+  if (error || !session || !session.user) {
+    console.error("Error fetching session or user is not logged in:", error);
+    window.location.href = "/loginUser"; // Redirigir al login si no está autenticado
+  }
+};
