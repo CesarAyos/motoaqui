8c9e615b0ff08151d4c97b7cea572b6e1b5e1ac9
ayos <ayos007@gmail.com>
2025-04-07 11:11:40 -0400

    modify

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,7 @@
-<!-- <nav class="navbar bg-body-tertiary" style="box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);">
+<nav class="navbar bg-body-tertiary" style="box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);">
     <div class="container">
       <a class="navbar-brand" href="/">
         <h1>Rapiditos Colón</h1>
       </a>
     </div>
-  </nav> -->
+  </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script>
   import LoginUser from "../components/loginUser.svelte";
-  import Navbar from "../components/navbar.svelte";
   
 
   
 </script>
-<Navbar />
+
 <LoginUser />
