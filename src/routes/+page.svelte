5cdc7a673623142d17f0c7b3e6b216f05043bf05
ayos <ayos007@gmail.com>
2025-04-12 18:42:36 -0400

    updow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,17 @@
   
 </script>
 
+
+
 <LoginUser />
+
+<div class="container d-flex flex-column align-items-center justify-content-center pb-5">
+  <h1>Clientes Rapiditos Colón</h1>
+  <p>Aplicación para la gestión de clientes Rapiditos Colón.</p>
+  <p>Descarga la aplicación en tu dispositivo Android.</p>
+<a href='https://clientes-rapiditos-colon.en.uptodown.com/android' title='Download Clientes Rapiditos Colón' >
+  <img src='https://stc.utdstc.com/img/mediakit/certified-free.png' alt='Download Clientes Rapiditos Colón'>
+  </a>
+</div>
+
+
